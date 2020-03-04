858c0e828ae9c2a40d997202998dcac62ad0b323
HaroldHS16 <haroldhs.hh@gmail.com>
2020-03-04 21:15:59 +0700

    Creating models

--- tugas/admin.py
+++ tugas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+#
+# Register 5 models : Car, Transaction, Category, Article, Review
+#
+admin.site.register(Car)
+admin.site.register(Transaction)
+admin.site.register(Category)
+admin.site.register(Article)
+admin.site.register(Review)
--- tugas/models.py
+++ tugas/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+#
+# There are 5 models : Car, Transaction, Category, Article, Review
+#
+
+class Car(models.Model):
+    carName = models.CharField(max_length=30)
+    carCategory = models.CharField(max_length=20)
+    carYear = models.CharField(max_length=4)
+    carCity = models.CharField(max_length=50)
+    carPrice = models.CharField(max_length=15)
+    carDescription = models.CharField(max_length=1000)
+class Transaction(models.Model):
+    carName = models.ForeignKey(Car)
+class Category(models.Model):
+    carCategory = models.ForeignKey(Car)
+class Article(models.Model):
+    carName = models.ForeignKey(Car)
+    articleContent = models.CharField(max_length=1000)
+class Review(models.Model):
+    carName = models.ForeignKey(Car)
