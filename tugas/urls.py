3fc555951a96fc776e3536209815ae435e206f7d
HaroldHS16 <haroldhs.hh@gmail.com>
2020-03-05 09:52:09 +0700

    Basic functions for urls.py and views.py

--- tugas/urls.py
+++ tugas/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='index'),
+    path('cars', views.cars, name='cars'),
+    path('carsView', views.carsView, name='carsView'),
+    path('about', views.about, name='about'),
+    path('articles', views.articles, name='articles')
 ]
--- tugas/views.py
+++ tugas/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render,redirect
 
 # Create your views here.
+
+#
+# Views for index.html
+#
 def home(request):
     return render(request,'pages/index.html',{})
+#
+# Views for cars.html
+#
+def cars(request):
+    return render(request, 'pages/cars.html')
+#
+# Views for carsView.html
+#
+def carsView(request):
+    return render(request, 'pages/carsView.html')
+#
+# Views for about.html
+#
+def about(request):
+    return render(request, 'pages/about.html')
+#
+# Views for articles.html
+#
+def articles(request):
+    return render(request, 'pages/articles.html')
