aa5793484bfb2cd6cfd283b935725f2824ac5852
Munirudeen  Maricar <maricar.munirudeen@gmail.com>
2020-03-05 16:56:07 +0700

    Adding Tests for URL and Templates

--- tugas/tests.py
+++ tugas/tests.py
@@ -4,45 +4,113 @@ from django.apps import apps
 from .apps import *
 from .views import *
 from .models import *
-from .forms import  
+from .forms import *
 
-# Create your tests here.
-class Story6UnitTest (TestCase):
-    def testStoryURL(self):
-        response = Client().get('/story6/')
+class GroupAssignmentUnitTest (TestCase):
+
+#-------------------------------------------------------------------------------- URL TESTING ----------------------------------------------------------------------------
+
+    def testHomePageURL(self):
+        response = Client().get('')
         self.assertEqual(response.status_code, 200)
 
-    def testStoryUsingTemplate(self):
-        response = Client().get('/story6/')
-        self.assertTemplateUsed(response, 'story.html')
+    def testCarsPageURL(self):
+        response = Client().get('/cars/')
+        self.assertEqual(response.status_code, 200)
 
-    def testStoryUsingHomeFunction(self):
-        found = resolve('/story6/')
-        self.assertEqual(found.func, home)
+    def testCarsViewPageURL(self):
+        response = Client().get('/CarsView/')
+        self.assertEqual(response.status_code, 200)
 
-    def testContainsGreeting(self):
-        response = Client().get('/story6/')
-        response_content = response.content.decode('utf-8')
-        self.assertIn("Hello, how are you?", response_content)
+    def testFindCarPageURL(self):
+        response = Client().get('/findCar/')
+        self.assertEqual(response.status_code, 200)
 
-    def testApps(self):
-        self.assertEqual(Story6Config.name, 'story6')
-        self.assertEqual(apps.get_app_config('story6').name, 'story6')
+    def testRentFormPageURL(self):
+            response = Client().get('/RentForm/')
+            self.assertEqual(response.status_code, 200)
+
+    def testSendRentFormPageURL(self):
+        response = Client().get('/sendRentForm/')
+        self.assertEqual(response.status_code, 200)
+
+    def testArticlesPageURL(self):
+        response = Client().get('/articles/')
+        self.assertEqual(response.status_code, 200)
+
+    def testChooseArticlePageURL(self):
+        response = Client().get('/chooseArticle/')
+        self.assertEqual(response.status_code, 200)  
+
+    def testAboutPageURL(self):
+        response = Client().get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+#------------------------------------------------------------------------------ TEMPLATE TESTING -------------------------------------------------------------------------
     
-    def testModelCreateNewStatus(self): 
-        newStatus = Status.objects.create(name='Munir', status='Happy')
-        countsAllNewStatus = Status.objects.all().count()
-        self.assertEqual(countsAllNewStatus, 1)
+    def testHomePageUsingTemplate(self):
+        response = Client().get('')
+        self.assertTemplateUsed(response, 'pages/index.html')
+
+    def testCarsPageUsingTemplate(self):
+        response = Client().get('/cars/')
+        self.assertTemplateUsed(response, 'pages/cars.html')
+
+    def testCarsViewPageUsingTemplate(self):
+        response = Client().get('/CarsView/')
+        self.assertTemplateUsed(response, 'pages/carsView.html')
+
+    def testFindCarPageUsingTemplate(self):
+        response = Client().get('/findCar/')
+        self.assertTemplateUsed(response, 'pages/findCar.html')
+
+    def testRentFormPageUsingTemplate(self):
+        response = Client().get('/RentForm/')
+        self.assertTemplateUsed(response, 'pages/rentForm.html')
+
+    def testSendRentFormPageUsingTemplate(self):
+        response = Client().get('/sendRentForm/')
+        self.assertTemplateUsed(response, 'pages/sendRentForm.html')
+
+    def testArticlesPageUsingTemplate(self):
+        response = Client().get('/articles/')
+        self.assertTemplateUsed(response, 'pages/articles.html')
+
+    def testChooseArticlePageUsingTemplate(self):
+        response = Client().get('/chooseArticle/')
+        self.assertTemplateUsed(response, 'pages/chooseArticle.html')
+
+    def testAboutPageUsingTemplate(self):
+        response = Client().get('/about/')
+        self.assertTemplateUsed(response, 'pages/about.html')
+
+    # def testStoryUsingHomeFunction(self):
+    #     found = resolve('/story6/')
+    #     self.assertEqual(found.func, home)
+
+    # def testContainsGreeting(self):
+    #     response = Client().get('/story6/')
+    #     response_content = response.content.decode('utf-8')
+    #     self.assertIn("Hello, how are you?", response_content)
+
+    # def testApps(self):
+    #     self.assertEqual(Story6Config.name, 'story6')
+    #     self.assertEqual(apps.get_app_config('story6').name, 'story6')
     
-    def testModelReturnsStatusAttributes(self):
-        newStatus = Status.objects.create(name='Munir', status='Happy')
-        self.assertEqual(str(newStatus.name), newStatus.name)
-        self.assertEqual(str(newStatus.status), newStatus.status)
+    # def testModelCreateNewStatus(self): 
+    #     newStatus = Status.objects.create(name='Munir', status='Happy')
+    #     countsAllNewStatus = Status.objects.all().count()
+    #     self.assertEqual(countsAllNewStatus, 1)
+    
+    # def testModelReturnsStatusAttributes(self):
+    #     newStatus = Status.objects.create(name='Munir', status='Happy')
+    #     self.assertEqual(str(newStatus.name), newStatus.name)
+    #     self.assertEqual(str(newStatus.status), newStatus.status)
 
-    def testStatusFormSoNotBlank(self):
-        newStatus = StatusForm({'name':'', 'status':''})
-        self.assertFalse(newStatus.is_valid())
+    # def testStatusFormSoNotBlank(self):
+    #     newStatus = StatusForm({'name':'', 'status':''})
+    #     self.assertFalse(newStatus.is_valid())
 
-    def testModelReturnsStatus(self):
-        newStatus = Status.objects.create(name='Munir', status='Happy')
-        self.assertEqual(str(newStatus), newStatus.status)
+    # def testModelReturnsStatus(self):
+    #     newStatus = Status.objects.create(name='Munir', status='Happy')
+    #     self.assertEqual(str(newStatus), newStatus.status)
--- tugas/urls.py
+++ tugas/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='index'),
-    path('findCar', views.findCar, name='findCar'),
-    path('chooseArticle', views.chooseArticle, name='chooseArticle'),
-    path('sendRentForm', views.sendRentForm, name='sendRentForm'),
-    path('about/',views.about,name='about'),
-    path('articles/',views.articles,name='articles'),
     path('cars/',views.cars,name='cars'),
     path('CarsView/',views.carsView,name='carsView'),
+    path('findCar/', views.findCar, name='findCar'),
     path('RentForm/',views.rentForm,name='rentForm'),
+    path('sendRentForm/', views.sendRentForm, name='sendRentForm'),
+    path('articles/',views.articles,name='articles'),
+    path('chooseArticle/', views.chooseArticle, name='chooseArticle'),
+    path('about/',views.about,name='about'),
 ]
